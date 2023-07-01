9613e70a8f74e8fecd463593993dbf26b8d8fd3f
priyesh kumar <59879071+priyeshsingh535@users.noreply.github.com>
2023-07-01 23:42:03 +0530

    EP_02

---
+++ episode_02/App.js
@@ -0,0 +1,21 @@
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+
+ 
+ 
+ 
+ const heading = React.createElement("div", {
+    id:"parent"
+},React.createElement("div",{id:"child"},
+[React.createElement("h1", {}," I am h1 tag"),
+React.createElement("h2", {}, " I am h2 tag")]),
+
+
+React.createElement("div",{id:"child"},
+[React.createElement("h1", {}," I am h1 tag"),
+React.createElement("h2", {}, " I am h2 tag")]));
+
+console.log(heading);  //it is an object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
